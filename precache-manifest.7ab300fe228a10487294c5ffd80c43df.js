59777a492244d0e13176573e540b21342429be38
kento <patrick.stasch>
2020-06-01 01:19:16 +0200

    Updates

---
+++ precache-manifest.7ab300fe228a10487294c5ffd80c43df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596025927ccdd61005be2ec4739b90de",
+    "url": "/runez/index.html"
+  },
+  {
+    "revision": "86ddb57266fe08c57fdc",
+    "url": "/runez/static/css/main.14266398.chunk.css"
+  },
+  {
+    "revision": "66b0191ff4f2776d2f9b",
+    "url": "/runez/static/js/2.869f8b53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/runez/static/js/2.869f8b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ddb57266fe08c57fdc",
+    "url": "/runez/static/js/main.45275977.chunk.js"
+  },
+  {
+    "revision": "426e876564601250450a",
+    "url": "/runez/static/js/runtime-main.fd01f014.js"
+  }
+]);
